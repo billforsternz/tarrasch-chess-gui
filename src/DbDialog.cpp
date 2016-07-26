030e8ab7129562149bfb750d4867ab2e88fd656e
Bill Forster <billforsternz@gmail.com>
2016-07-27 11:15:35 +1200

    Dummy commit - used to note previous commit fixed longstanding semi-bug in StatsCalculate(); add IsEmpty() checks before InsertItems()

--- src/DbDialog.cpp
+++ src/DbDialog.cpp
@@ -2,7 +2,7 @@
  * Database data source for games dialog
  *  Author:  Bill Forster
  *  License: MIT license. Full text of license is in associated file LICENSE
- *  Copyright 2010-2014, Bill Forster <billforsternz at gmail dot com>
+ *  Copyright 2010-2016, Bill Forster <billforsternz at gmail dot com>
  ****************************************************************************/
 #include "wx/wx.h"
 #include "wx/valtext.h"
